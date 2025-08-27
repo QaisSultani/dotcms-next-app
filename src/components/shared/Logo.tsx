b3c734701691df3fa6e883d0052f8de6a75be97d
QaisSultani <qais.1601138@gmail.com>
2025-08-27 18:14:32 +0500

    fix: UI fix

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = ({ onClick }: { onClick?: (href: string) => void }) => {
   return (
     <a
       href="#hero"
-      className="flex justify-center items-center gap-1 text-2xl font-bold bg-gradient-to-r from-primary to-blue-600 bg-clip-text text-transparent"
+      className="flex items-center gap-1 text-2xl font-bold bg-gradient-to-r from-primary to-blue-600 bg-clip-text text-transparent"
       onClick={(e) => {
         e.preventDefault();
         if (onClick) onClick("#hero");
